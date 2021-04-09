5de20a45d654e8483778768ec3b63ba240432054
manassehl9 <manassehl9@gmail.com>
2021-04-09 02:07:31 +0100

    feat: error handling

---
+++ server/Response/index.js
@@ -0,0 +1,9 @@
+const badRequest = require('./codes/bad-request')
+const created = require('./codes/created')
+const success = require('./codes/success')
+const validationError = require('./codes/validation-error')
+
+module.exports.badRequest = badRequest
+module.exports.created = created
+module.exports.success = success
+module.exports.validationError = validationError
--- server/controllers/ShapeController.js
+++ server/controllers/ShapeController.js
@@ -1,14 +1,16 @@
+const { created, badRequest, validationError } = require("../Response/index");
 const ShapeService = require("../services/ShapeService");
-
 class ShapeController {
   static async calculateShape(req, res) {
     let total;
     let data;
 
     try {
-      if (!req.body.shape) return;
+      if (!req.body.shape)
+        return badRequest(res, `Please provide the shape`, null);
 
-      if (!req.body.dimensions) return;
+      if (!req.body.dimensions)
+        return badRequest(res, `Please provide the dimensions`, null);
 
       const shape = req.body.shape.trim().toUpperCase();
 
@@ -17,9 +19,19 @@ class ShapeController {
         case "SQUARE":
           const { sideA, sideB } = req.body.dimensions;
 
-          if (!sideA || !sideB) return;
+          if (!sideA || !sideB)
+            return badRequest(
+              res,
+              `Please provide the value for sideA and sideB`,
+              null
+            );
 
-          if (!Number.isFinite(sideA) || !Number.isFinite(sideB)) return;
+          if (!Number.isFinite(sideA) || !Number.isFinite(sideB))
+            return validationError(
+              res,
+              `sideA and sideB requires a number`,
+              null
+            );
 
           total = sideA * sideB;
 
@@ -30,16 +42,25 @@ class ShapeController {
           };
 
           await ShapeService.saveCalculation(data);
-          console.log(res);
-          break;
+          return created(res, `Square calculation saved successfully!`, data);
 
         // Calculate area of rectangle
         case "RECTANGLE":
           const { length, breadth } = req.body.dimensions;
 
-          if (!length || !breadth) return;
+          if (!length || !breadth)
+            return badRequest(
+              res,
+              `Please provide the value for length and breadth`,
+              null
+            );
 
-          if (!Number.isFinite(length) || !Number.isFinite(breadth)) return;
+          if (!Number.isFinite(length) || !Number.isFinite(breadth))
+            return validationError(
+              res,
+              `"length and breadth requires a number`,
+              null
+            );
 
           total = length * breadth;
           data = {
@@ -50,21 +71,33 @@ class ShapeController {
 
           await ShapeService.saveCalculation(data);
 
-          console.log(res);
-          break;
+          return created(
+            res,
+            `rectangle calculation saved successfully!`,
+            data
+          );
 
         // Calculate area of triangle
         case "TRIANGLE":
           const { length_a, length_b, length_c } = req.body.dimensions;
 
-          if (!length_a || !length_a || !length_c) return;
+          if (!length_a || !length_a || !length_c)
+            return badRequest(
+              res,
+              `Please provide the value for length_a,length_b and length_c`,
+              null
+            );
 
           if (
             !Number.isFinite(length_a) ||
             !Number.isFinite(length_b) ||
             !Number.isFinite(length_c)
           )
-            return;
+            return validationError(
+              res,
+              `length_a,length_b and length_c requires a number`,
+              null
+            );
 
           // calculate the semi-perimeter
           const s = (length_a + length_b + length_c) / 2;
@@ -85,15 +118,16 @@ class ShapeController {
           };
 
           await ShapeService.saveCalculation(data);
-          console.log(res);
-          break;
+          return created(res, `triangle calculation saved successfully!`, data);
 
         // Calculate area of circle
         case "CIRCLE":
           const { radius } = req.body.dimensions;
-          if (!radius) return;
+          if (!radius)
+            return badRequest(res, `Please provide the value for radius`, null);
 
-          if (!Number.isFinite(radius)) return;
+          if (!Number.isFinite(radius))
+            return validationError(res, `lradius requires a number`, null);
 
           total = Math.PI * (radius * radius);
           data = {
@@ -105,14 +139,13 @@ class ShapeController {
           };
 
           await ShapeService.saveCalculation(data);
-          console.log(res);
-          break;
+          return created(res, `circle calculation saved successfully!"`, data);
 
         default:
-          break;
+          return badRequest(res, `Bad Request`, null);
       }
     } catch (error) {
-      console.log(error);
+      return badRequest(res, `${error.message}`, null);
     }
   }
 }
