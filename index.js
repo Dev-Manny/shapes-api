634194a937aa3a3de58d77aa088fb69c12303991
manassehl9 <manassehl9@gmail.com>
2021-04-09 04:49:25 +0100

    docs: adding swagger docs wip

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const app = express();
 const shapeRoutes = require("./server/routes/ShapeRoutes");
 const userRoutes = require("./server/routes/UserRoutes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 app.use(express.json());
 
 const port = process.env.PORT || 8000;
 app.use("/api/v1/calculate/", shapeRoutes);
 app.use("/api/v1/user/", userRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// when a random route is inputed
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to this API.",
+  })
+);
 
 app.listen(port, console.log(`Server started on ${port}`));
